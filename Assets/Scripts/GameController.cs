99a0569d9838e105cbcb1392e772e3146f4afa18
yngnicole <nicoley5@uci.edu>
2026-02-02 19:23:23 -0800

    s

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,8 +8,15 @@ public class GameController : MonoBehaviour
 {
     [SerializeField] private GameObject _topPipePrefab;
     [SerializeField] private GameObject _bottomPipePrefab;
-    [SerializeField] private float _pipeGap = 3f;
-    [SerializeField] private float _spawnX = 10f; //spawn location
+   // [SerializeField] private float _pipeGap = 3f;
+   // [SerializeField] private float _spawnX = 10f; //spawn location
+  //  [SerializeField] private float _spawnInterval = 2f;
+
+    
+    [SerializeField] private float _spawnX = 10f;
+    [SerializeField] private float _gap = 5f;
+    [SerializeField] private float _minY = -2f;
+    [SerializeField] private float _maxY = 2f;
     [SerializeField] private float _spawnInterval = 2f;
 
     private float _spawnTimer;
@@ -27,32 +34,45 @@ public class GameController : MonoBehaviour
     public void SpawnPipes()
     {
 
-        float cameraHalfHeight = Camera.main.orthographicSize;
-        float cameraHalfWidth = cameraHalfHeight * Camera.main.aspect;
+        float centerY = UnityEngine.Random.Range(_minY, _maxY);
 
-        float pipeHeight = 10f;
-            //_bottomPipePrefab.GetComponent<SpriteRenderer>().bounds.size.y;
+        // Bottom pipe
+        Instantiate(_bottomPipePrefab, new Vector3(_spawnX, centerY - _gap, 0), Quaternion.identity);
+
+        // Top pipe
+        Instantiate(_topPipePrefab, new Vector3(_spawnX, centerY + _gap, 0), Quaternion.identity);
+
+
+        /*
+        float cameraHalfHeight = Camera.main.orthographicSize;
+        //float cameraHalfWidth = cameraHalfHeight * Camera.main.aspect;
+
+        float pipeHeight = _bottomPipePrefab.GetComponent<SpriteRenderer>().bounds.size.y;
 
         // vertical offset range
-        float maxOffset = cameraHalfHeight - pipeHeight - _pipeGap / 2f;
+        float maxOffset = cameraHalfHeight - _pipeGap / 2f - pipeHeight /2f;
         float offsetY = UnityEngine.Random.Range(-maxOffset, maxOffset);
-    
+
 
         //instantiate bottom pipe
-        float bottomY = -cameraHalfHeight + pipeHeight / 2f + offsetY;
+        float bottomY = offsetY - _pipeGap / 2f;
+        //-cameraHalfHeight + pipeHeight / 2f + offsetY;
         Instantiate(
             _bottomPipePrefab, 
             new Vector3(_spawnX, bottomY, 0), 
             UnityEngine.Quaternion.identity);
 
+
         //instantiate top pipe
-        float topY = cameraHalfHeight - pipeHeight / 2f + offsetY;
+        float topY = offsetY + _pipeGap / 2f;
+            //cameraHalfHeight - pipeHeight / 2f + offsetY;
         Instantiate(
             _topPipePrefab, 
             new Vector3(_spawnX, topY, 0), 
             UnityEngine.Quaternion.identity);
+        */
 
-        
+
     }
 
     
