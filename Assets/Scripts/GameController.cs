956f4b7e8f6335d4afa945caaa94d04b395fd3e4
yngnicole <nicoley5@uci.edu>
2026-02-02 19:59:03 -0800

    e

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,6 +8,7 @@ public class GameController : MonoBehaviour
 {
     [SerializeField] private GameObject _topPipePrefab;
     [SerializeField] private GameObject _bottomPipePrefab;
+    [SerializeField] private GameObject _pointsWall;
 
     [SerializeField] private float _spawnX = 10f;
     [SerializeField] private float _gap = 5f;
@@ -16,6 +17,24 @@ public class GameController : MonoBehaviour
     [SerializeField] private float _spawnInterval = 2f;
 
     private float _spawnTimer;
+
+    public static GameController Instance { get; private set; }
+    public YellowBird Player { get; private set; } //ref to player
+
+    public void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+
+        Instance = this;
+
+        GameObject playerObj = GameObject.FindWithTag("Player");
+        Player = playerObj.GetComponent<YellowBird>();
+    }
+
 
     private void Update()
     {
@@ -38,13 +57,12 @@ public class GameController : MonoBehaviour
         // Top pipe
         Instantiate(_topPipePrefab, new Vector3(_spawnX, centerY + _gap, 0), Quaternion.identity);
 
+        //points wall
+        Instantiate(_pointsWall, new Vector3(_spawnX + 1f, centerY, 0f), Quaternion.identity);
     }
 
     
 
-
-    //needs ref to player 
-    // singleton. gamecontroller is locator
     // subs to ui and audio
     // The score code uses a Singleton to subscribe to any events raised.
 }
