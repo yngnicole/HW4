d5d2a29008ec4ed86c1b3a4b905905045752b486
yngnicole <nicoley5@uci.edu>
2026-02-02 19:28:34 -0800

    s

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,11 +8,7 @@ public class GameController : MonoBehaviour
 {
     [SerializeField] private GameObject _topPipePrefab;
     [SerializeField] private GameObject _bottomPipePrefab;
-   // [SerializeField] private float _pipeGap = 3f;
-   // [SerializeField] private float _spawnX = 10f; //spawn location
-  //  [SerializeField] private float _spawnInterval = 2f;
 
-    
     [SerializeField] private float _spawnX = 10f;
     [SerializeField] private float _gap = 5f;
     [SerializeField] private float _minY = -2f;
@@ -41,37 +37,6 @@ public class GameController : MonoBehaviour
 
         // Top pipe
         Instantiate(_topPipePrefab, new Vector3(_spawnX, centerY + _gap, 0), Quaternion.identity);
-
-
-        /*
-        float cameraHalfHeight = Camera.main.orthographicSize;
-        //float cameraHalfWidth = cameraHalfHeight * Camera.main.aspect;
-
-        float pipeHeight = _bottomPipePrefab.GetComponent<SpriteRenderer>().bounds.size.y;
-
-        // vertical offset range
-        float maxOffset = cameraHalfHeight - _pipeGap / 2f - pipeHeight /2f;
-        float offsetY = UnityEngine.Random.Range(-maxOffset, maxOffset);
-
-
-        //instantiate bottom pipe
-        float bottomY = offsetY - _pipeGap / 2f;
-        //-cameraHalfHeight + pipeHeight / 2f + offsetY;
-        Instantiate(
-            _bottomPipePrefab, 
-            new Vector3(_spawnX, bottomY, 0), 
-            UnityEngine.Quaternion.identity);
-
-
-        //instantiate top pipe
-        float topY = offsetY + _pipeGap / 2f;
-            //cameraHalfHeight - pipeHeight / 2f + offsetY;
-        Instantiate(
-            _topPipePrefab, 
-            new Vector3(_spawnX, topY, 0), 
-            UnityEngine.Quaternion.identity);
-        */
-
 
     }
 
