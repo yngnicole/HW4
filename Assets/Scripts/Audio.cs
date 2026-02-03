0d34bf3a9f3f461d08ae4dbe16bbfe37445aa3c3
yngnicole <nicoley5@uci.edu>
2026-02-02 21:15:38 -0800

    audio

--- Assets/Scripts/Audio.cs
+++ Assets/Scripts/Audio.cs
@@ -4,12 +4,47 @@ using UnityEngine;
 
 public class Audio : MonoBehaviour
 {
-    [SerializeField] 
-    [SerializeField]
-    [SerializeField]
-    [SerializeField]
-    // audio soruce 
-    // events plays audio
-    // The score text AND audio system is alerted of changes to the Player’s score via events.
-    // sub to events
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _flapClip;
+    [SerializeField] private AudioClip _scoreClip;
+    [SerializeField] private AudioClip _hitClip;
+
+    private void OnEnable()
+    {
+        if (GameController.Instance != null)
+        {
+            GameController.Instance.OnScoreChanged += HandleScoreChanged;
+            GameController.Instance.OnGameOver += HandleGameOver;
+            GameController.Instance.OnPlayerFlapped += HandleFlap;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (GameController.Instance != null)
+        {
+            GameController.Instance.OnScoreChanged -= HandleScoreChanged;
+            GameController.Instance.OnGameOver -= HandleGameOver;
+            GameController.Instance.OnPlayerFlapped -= HandleFlap;
+        }
+    }
+
+    private void HandleFlap()
+    {
+        if (_flapClip != null)
+            _audioSource.PlayOneShot(_flapClip);
+    }
+
+    private void HandleScoreChanged(int newScore)
+    {
+        if (_scoreClip != null)
+            _audioSource.PlayOneShot(_scoreClip);
+    }
+
+    private void HandleGameOver()
+    {
+        if (_hitClip != null)
+            _audioSource.PlayOneShot(_hitClip);
+    }
+
 }
